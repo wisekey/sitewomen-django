177d1b333fd6a58c133cfe9bbc19237b956b3350
wisekey <bekov.tembulat.g@gmail.com>
2025-07-08 22:35:45 +0300

    Practice with custom path converters, learn path converters

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from women import views
 
 urlpatterns = [
     path('/admin', admin.site.urls),
-    path('/index', views.index),
-    path('/categories', views.categories)
+    path('', include('women.urls')),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,5 +5,13 @@ def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the women's index")
 
 
-def categories(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request: HttpRequest, cat_id: int) -> HttpResponse:
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id : {cat_id}</p>")
+
+
+def categories_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request: HttpRequest, year: int) -> HttpResponse:
+    return HttpResponse(f"<h1>Архив по годам</h1><p>Год издания: {year}</p>")
