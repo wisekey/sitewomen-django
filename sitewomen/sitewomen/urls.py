ff4dec0ad1e6be28e47acbacc6c4e8e182369d01
wisekey <bekov.tembulat.g@gmail.com>
2025-07-08 16:57:43 +0300

    Update urls.py

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from women import views
 
 urlpatterns = [
     path('/admin', admin.site.urls),
-    path('women/', include('women.urls')),
+    path('/index', views.index),
+    path('/categories', views.categories)
 ]
