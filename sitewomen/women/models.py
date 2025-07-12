b30a87216de6edd78269283af06c2115df3b88c1
wisekey <bekov.tembulat.g@gmail.com>
2025-07-12 19:30:00 +0300

    Create model Women

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
+    
+
+class Women(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
+
+    def get_absolute_url(self):
+        return reverse("post", kwargs={"post_slug": self.slug,})
