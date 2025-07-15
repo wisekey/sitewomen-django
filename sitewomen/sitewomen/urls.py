63bc65569ef7706ea2faff5d10dbcd04fcd53423
wisekey <bekov.tembulat.g@gmail.com>
2025-07-15 18:13:48 +0300

    debug toolbar support

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -5,6 +5,7 @@ from women.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('', include('women.urls')),
 ]
 
