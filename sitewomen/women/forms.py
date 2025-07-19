5d032a07d64cddf5963e473e93bf76b8ef920900
wisekey <bekov.tembulat.g@gmail.com>
2025-07-19 16:12:39 +0300

    add FormModel class

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -1,11 +1,40 @@
 from django import forms
-from .models import Category, Husband
+from .models import Category, Husband, Women
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.exceptions import ValidationError
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+class AddPostForm(forms.ModelForm):
+    
+    cat = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        label='Категории',
+        empty_label='Категория не выбрана',
+    )
+    husband = forms.ModelChoiceField(
+        queryset=Husband.objects.all(),
+        required=False,
+        label='Муж',
+        empty_label='Не замужем',
+    )    
+
+    class Meta:
+        model = Women
+        fields = ("title", "slug", "content", "is_published", "cat", "husband", "tags")
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-input"}),
+            "content": forms.Textarea(attrs={"cols": 50, "rows": 5})
+        }
+        labels = {
+            "slug": "URL"
+        }
+        
+    
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+
+        if len(title) > 50:
+            raise ValidationError("Заголовок не может быть более 50 символов")
+        
+        return title
+    
