8bc2afa439538c70ba3d3249b662550bc9cbb5ab
wisekey <bekov.tembulat.g@gmail.com>
2025-07-20 20:53:51 +0300

    add class instead functions views

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -2,16 +2,15 @@ from django.urls import path, re_path, register_converter
 from . import views
 from . import converters
 
-
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.WomenHome.as_view(), name='home'),
     path('about/', views.about, name='about'),
-    path('addpage/', views.addpage, name="add_page"),
+    path('addpage/', views.AddPage.as_view(), name="add_page"),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
-    path('category/<slug:cat_slug>', views.show_category, name="category"),
+    path('category/<slug:cat_slug>', views.WomenCategory.as_view(), name="category"),
     path('post/<slug:post_slug>/', views.show_post, name='post'),
     path('tag/<slug:tag_slug>', views.show_tag_postlit, name='tag'),
 ]
