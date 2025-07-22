caa34801d4d82e2e60cc15e614978636d8536d1b
wisekey <bekov.tembulat.g@gmail.com>
2025-07-22 19:49:18 +0300

    paginate update

--- sitewomen/women/utils.py
+++ sitewomen/women/utils.py
@@ -22,7 +22,7 @@ class DataMixin:
     title_page = None
     extra_context = {}
     cat_selected = None
-    paginate_by = 3
+    paginate_by = 5
 
     def __init__(self):
         if self.title_page:
