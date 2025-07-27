f0518128c827d30877b866bf52f9935cb1c71cf5
wisekey <bekov.tembulat.g@gmail.com>
2025-07-27 09:49:42 +0300

    add some tests

--- sitewomen/women/tests.py
+++ sitewomen/women/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
+from .models import Women
 
-# Create your tests here.
+
+class GetPagesTestCase(TestCase):
+    fixtures = ["women_women.json", "women_category.json", "women_husband.json", "women_tagpost.json", "users_user.json"]
+
+    def setUp(self):
+        return super().setUp()
+    
+    def test_main_page(self):
+        path = reverse("home")
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "women/index.html")
+        self.assertEqual(response.context_data["title"], "Главная страница")
+
+
+    def test_redirect_add_page(self):
+        path = reverse("add_page")
+        redirect_url = reverse("users:login") + "?next=" + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_url)
+
+    def test_data_mainpage(self):
+        w = Women.published.all().select_related("cat")
+        path = reverse("home")
+        response = self.client.get(path)
+        print(w)
+
+    def test_case_2(self):
+        pass
+
+    def tearDown(self):
+        return super().tearDown()
